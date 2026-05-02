e059faaeaaea13cf716f121a61a6fceaa7c3c546
bslrd <busler.dominik@gmail.com>
2026-05-02 22:50:20 +0200

    Apply .gitignore

--- Debug/libs/snake/snake.d
+++
@@ -1,4 +0,0 @@
-libs/snake/snake.o: ../libs/snake/snake.c ../libs/snake/snake.h \
- /Users/dominik/Desktop/STM/WORKSPACE/LED_MATRIX/libs/2dspace/2dspace.h
-../libs/snake/snake.h:
-/Users/dominik/Desktop/STM/WORKSPACE/LED_MATRIX/libs/2dspace/2dspace.h:
--- Debug/libs/ws2812b/ws2812b_matrix_symbols.d
+++
@@ -1,4 +0,0 @@
-libs/ws2812b/ws2812b_matrix_symbols.o: \
- ../libs/ws2812b/ws2812b_matrix_symbols.c \
- ../libs/ws2812b/ws2812b_matrix.h
-../libs/ws2812b/ws2812b_matrix.h:
